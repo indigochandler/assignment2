a859cd69dc93e84a7af93df443e713588ed1c777
Indigo Chandler <indigochandler@gmail.com>
2025-03-18 13:15:40 +0000

    removing rows where both T3 and T4 are entirely missing

--- my_assignment_2.py
+++ my_assignment_2.py
@@ -7,3 +7,10 @@ import os
 
 sample_data = pd.read_csv("P2data1035.csv")
 sample_data.head(10) 
+
+data = pd.read_csv("P2data1035.csv", delimiter="\t")
+data.head(10) 
+
+print("Rows with missing values for 'T4' and 'T3':")
+print(data[data[['T3', 'T4']].isna().all(axis=1)]) 
+data = data.dropna(subset=['T3', 'T4'], how='all') 
