274c90d99b00a2d8a35be8d34f86cebae7c4c492
Indigo Chandler <indigochandler@gmail.com>
2025-03-18 13:20:25 +0000

    replacing rows with missing values for T3 or T4 are replaced with the mean for their respective level

--- my_assignment_2.py
+++ my_assignment_2.py
@@ -14,3 +14,9 @@ data.head(10)
 print("Rows with missing values for 'T4' and 'T3':")
 print(data[data[['T3', 'T4']].isna().all(axis=1)]) 
 data = data.dropna(subset=['T3', 'T4'], how='all') 
+
+data['T3'] = data['T3'].fillna(data.groupby('Level')['T3'].transform('mean')).round(1)
+data['T4'] = data['T4'].fillna(data.groupby('Level')['T4'].transform('mean')).round(1)
+data[['T3', 'T4']].isnull().sum()
+
+
